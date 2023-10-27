c7672a2128c691c21271bf42bc260d906de70913
bfalad22 <bolasaliu93@gmail.com>
2023-10-27 05:55:44 -0700

    Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,22 +1,29 @@
-"""
-    Data: a list of integers
-    Return: True if data is a vlid UTF-8
-    encoding, else return False
-"""
-    byte_count = 0
+#!/usr/bin/python3
+"""UTF-8 Validation"""
+
+
+def validUTF8(data):
+    """
+    method that determines if a given data set
+    represents a valid UTF-8 encoding
+    """
+    nbytes = 0
+
+    b1 = 1 << 7
+    b2 = 1 << 6
 
     for i in data:
-        if byte_count == 0:
-            if i >> 5 == 0b110 or i >> 5 == 0b1110:
-                byte_count = 1
-            elif i >> 4 == 0b1110:
-                byte_count = 2
-            elif i >> 3 == 0b11110:
-                byte_count = 3
-            elif i >> 7 == 0b1:
+        b = 1 << 7
+        if nbytes == 0:
+            while b & i:
+                nbytes += 1
+                b = b >> 1
+            if nbytes == 0:
+                continue
+            if nbytes == 1 or nbytes > 4:
                 return False
         else:
-            if i >> 6 != 0b10:
+            if not (i & b1 and not (i & b2)):
                 return False
-            byte_count -= 1
-            return byte_count == 0
+        nbytes -= 1
+    return nbytes == 0
